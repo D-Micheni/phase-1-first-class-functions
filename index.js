8478f8cd9f41af3f25677fcecf77d60f71ab356a
D-Micheni <dennis.micheni@student.moringaschool.com>
2022-08-18 22:13:41 +0300

    Done.

---
+++ index.js
@@ -0,0 +1,14 @@
+function receivesAFunction(callback) {
+    callback();
+  }
+  
+  function returnsANamedFunction() {
+    return function named() {
+      return 'before all'
+    };
+  }
+  
+  function returnsAnAnonymousFunction() {
+    return () => 'before all';
+  }
+Footer
